4c0b5675a8793f0ed4efbc8c74b6a2748430783b
Rafael <rafael-srv@hotmail.com>
2022-09-29 22:07:48 -0300

    Subindo Teste

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import {useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  let [Resultado, setResultado] = useState(0)
+  let [Message, setMessage] = useState("")
+  let [visible, setVisible] = useState(false)
+  
+
+  let zerar = function(){
+    
+    Message="";
+    setVisible(visible=false)
+    console.log(Message)
+    
+
+    
+  }
+
+  if (visible === true){
+    setTimeout((zerar), 2000)
+    
+  }
+  
+  
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div>
+    <p className="Resultado">{Resultado}</p>
+
+      
+      <div className="DivCentral">
+      {visible===true && (<div className="Mensagem">{Message}</div>)}  
+      <button id="mais" onClick={()=>{setResultado(Resultado+1); setMessage(Message="Incrementando 1"); setVisible(visible=true)}}>Mais</button>
+      <button id="menos" onClick={()=>{setResultado(Resultado-1); setMessage(Message="Decrementando 1"); setVisible(visible=true)}}>Menos</button>
+      <button id="zero" onClick={()=>{setResultado(Resultado=0); setMessage(Message="Zerando Total"); setVisible(visible=true)}}>Zerar</button>
+      
     </div>
-  );
+    </div>
+  )
+  
 }
 
 export default App;
