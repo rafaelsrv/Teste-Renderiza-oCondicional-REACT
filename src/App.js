f1b78da6a41a6e0c29f63083b2675f54d20592bd
Rafael <rafael-srv@hotmail.com>
2022-10-05 23:59:00 -0300

    Buscando dados da API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-
+import api from './components/Api';
 import './App.css';
-import estados from './components/Estados';
-import Contador from './components/Contador';
+//import estados from './components/Estados';
+//import Contador from './components/Contador';
+import React, {Component} from 'react';
 
-function App() {
+class App extends Component{
   
-  
-  
+  state={
+    filmes:[],      //Array onde serão guardadas as respostas, no caso os filmes que serão recebidos.
+  }
+  async componentDidMount(){
+    const response = await api.get('');     //O método GET irá procurar na api, o resultado será a resposta.
 
-  return (
     
-      <div>
-    <Contador/>
     
-    </div>
-    
-  
+    this.setState({ filmes: response.data})
+  }
 
-  )
-  
-  
+  render(){
+
+    const {filmes} = this.state;
+    return(
+   
+   <div>
+      <h1>Listar Filmes</h1>
+      
+      {filmes.map(filme => (
+        <li key={filme.show.id}>
+        <h2><strong>Título:</strong>
+        {filme.show.name}
+        </h2>
+        <p>{filme.show.url}</p>
+        
+        </li>
+      ))}
+    </div>)
+  }
 }
+
+
 
 export default App;
---
+++ src/components/Api.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+const api = axios.create({
+    baseURL: 'https://api.tvmaze.com/search/shows?q=star%20wars',
+
+})
+
+
+export default api
